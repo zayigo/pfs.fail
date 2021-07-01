9dfb90ab633dcd95cf8d8882cab4b753d291ee10
Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>
2021-07-01 10:24:31 +0200

    New image builder

--- buildtools/builder.sh
+++ buildtools/builder.sh
@@ -4,6 +4,6 @@ docker build -f buildtools/Dockerfile.libvips -t arl.fail:libvips .
 # Build Jekyll container & generate static files
 docker build -f buildtools/Dockerfile.builder -t arl.fail:builder .
 docker run --rm --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/photo-stream/_site" arl.fail:builder
-rm -rf $PWD/vendor/bundle/buildtools $PWD/vendor/bundle/README.md $PWD/vendor/bundle/Gemfile* $PWD/vendor/bundle/LICENSE $PWD/vendor/bundle/.git*
+sudo rm -rf $PWD/vendor/bundle/buildtools $PWD/vendor/bundle/README.md $PWD/vendor/bundle/Gemfile* $PWD/vendor/bundle/LICENSE $PWD/vendor/bundle/.git*
 # Build nginx container
 docker build -f buildtools/Dockerfile.nginx -t arl.fail:nginx .
