363b457f229545098bd40b63b6ac17b85bafe29f
Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>
2021-07-02 21:34:10 +0200

    builder

--- buildtools/builder.sh
+++ buildtools/builder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Build VIPS container
-docker build -f buildtools/Dockerfile.libvips -t arl.fail:libvips .
+#docker build -f buildtools/Dockerfile.libvips -t arl.fail:libvips .
 # Build Jekyll container & generate static files
 docker build -f buildtools/Dockerfile.builder -t arl.fail:builder .
 docker run --rm --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/photo-stream/_site" arl.fail:builder
